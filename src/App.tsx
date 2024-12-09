40e9e4761770a0e597bb699458cb78c6e6f7a92b
Lucas Leonardo <lucasleonardo@protonmail.com>
2024-12-09 20:48:44 -0300

    app: add time thing backk

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,36 @@ import Papa from "papaparse";
 const THIRTY_MINS = 30 * 60_000;
 
 type CernData = {
-	data: { Time: string; alice: string }[];
+	data: { Time: string; alice: number }[]; // TODO: don't hardcode these too much
 	lastDatapointIndex: number;
 	dataDelta: number;
 	timeDelta: number;
 };
+
+function update(
+	cernData: CernData,
+	setCernData: React.Dispatch<React.SetStateAction<CernData>>,
+) {
+	for (
+		let i = cernData.lastDatapointIndex;
+		i < cernData.data.length - 1;
+		i++
+	) {
+		const previousVal = cernData.data[i - 1]["alice"];
+		const val = cernData.data[i]["alice"];
+		const delta = Math.abs(val - previousVal);
+
+		if (delta >= cernData.dataDelta) {
+			console.log("delta:", delta, " at point", i);
+			setCernData((cd) => ({ ...cd, lastDatapointIndex: i + 1 }));
+			return;
+		}
+	}
+}
+
+function sleep(ms: number) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 function App() {
 	const [computeData, setComputeData] = useState<CernData>({
@@ -30,10 +55,14 @@ function App() {
 		);
 	}, []);
 
+	useEffect(() => {
+		setTimeout(() => update(computeData, setComputeData), 2_000);
+	}, [computeData]);
+
 	return (
 		<ul>
 			{computeData.data.map((d) => (
-				<li>{d["alice"] as string}</li>
+				<li>{d["alice"]}</li>
 			))}
 		</ul>
 	);
