0c7c2850651979ce3b97bf91475ce7d25e8129e4
Lucas Leonardo <lucasleonardo@protonmail.com>
2024-12-13 20:20:07 -0300

    misc: add ui with hardcoded values

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 // import Papa from "papaparse";
 // import Grid from "./assets/image.png";
 import CircleAnimation from "./components/CircleAnimation";
+import "./App.css";
 
 // const THIRTY_MINS = 30 * 60_000;
 
@@ -34,49 +35,116 @@ import CircleAnimation from "./components/CircleAnimation";
 // }
 
 function App() {
-  //   const [computeData, setComputeData] = useState<CernData>({
-  //     data: [],
-  //     lastDatapointIndex: 1,
-  //     dataDelta: 9_000,
-  //     timeDelta: THIRTY_MINS,
-  //   });
+	//   const [computeData, setComputeData] = useState<CernData>({
+	//     data: [],
+	//     lastDatapointIndex: 1,
+	//     dataDelta: 9_000,
+	//     timeDelta: THIRTY_MINS,
+	//   });
 
-  //   useEffect(() => {
-  //     Papa.parse(
-  //       "https://raw.githubusercontent.com/karen-pal/mandelgrid/refs/heads/main/js/batch-jobs-running.csv",
-  //       {
-  //         download: true,
-  //         header: true,
-  //         complete: ({ data }) => setComputeData((cd) => ({ ...cd, data })),
-  //       }
-  //     );
-  //   }, []);
+	//   useEffect(() => {
+	//     Papa.parse(
+	//       "https://raw.githubusercontent.com/karen-pal/mandelgrid/refs/heads/main/js/batch-jobs-running.csv",
+	//       {
+	//         download: true,
+	//         header: true,
+	//         complete: ({ data }) => setComputeData((cd) => ({ ...cd, data })),
+	//       }
+	//     );
+	//   }, []);
 
-  //   useEffect(() => {
-  //     setTimeout(() => update(computeData, setComputeData), 2_000);
-  //   }, [computeData]);
+	//   useEffect(() => {
+	//     setTimeout(() => update(computeData, setComputeData), 2_000);
+	//   }, [computeData]);
 
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100vw",
-        height: "100vh",
-      }}
-    >
-      {/* <CircleAnimation color="#ff9706" time={2000} /> */}
-      <CircleAnimation color="#0017e2" time={800} />
-
-      {/* <ul>
-        {computeData.data.map((d) => (
-          <li>{d["alice"]}</li>
-        ))}
-      </ul> */}
-    </div>
-  );
+	return (
+		<div
+			style={{
+				display: "grid",
+				gridTemplateColumns: "repeat(3, 1fr)",
+				gridTemplateRows: "repeat(3, 1fr)",
+				justifyContent: "center",
+				alignItems: "center",
+				width: "100vw",
+				height: "100vh",
+			}}
+		>
+			<div
+				id="stats"
+				style={{
+					gridRow: "2",
+					gridColumn: "1",
+					justifySelf: "center",
+					zIndex: "10",
+					display: "grid",
+					gap: "2rem",
+				}}
+			>
+				<div>
+					<p>
+						<strong>Users</strong>
+					</p>
+					<p>20,000</p>
+				</div>
+				<div>
+					<p>
+						<strong>Storage</strong>
+					</p>
+					<p>50,000 Pb</p>
+				</div>
+				<div>
+					<p>
+						<strong>RAM</strong>
+					</p>
+					<p>32,000 Tb</p>
+				</div>
+			</div>
+			{/* <CircleAnimation color="#ff9706" time={2000} /> */}
+			<div style={{ gridColumn: "1 / -1", gridRow: "1 / -1" }}>
+				<CircleAnimation color="#0017e2" time={800} />
+			</div>
+			<div
+				id="deltas"
+				style={{
+					gridColumn: "3",
+					gridRow: "1",
+					zIndex: 10,
+					display: "flex",
+					gap: "3rem",
+				}}
+			>
+				<div>
+					<p className="text-orange">
+						<strong>Network</strong>
+					</p>
+					<p>30 Mb</p>
+				</div>
+				<div>
+					<p className="text-blue">
+						<strong>Compute</strong>
+					</p>
+					<p>8,000</p>
+				</div>
+			</div>
+			<div
+				id="credits"
+				style={{
+					gridColumn: "1",
+					gridRow: "3",
+					justifySelf: "center",
+					alignSelf: "center",
+					zIndex: "10",
+				}}
+			>
+				<p>
+					<strong>Equípo técnico:</strong>
+				</p>
+				<p>Karen Palacio</p>
+				<p>Natalí Palacio</p>
+				<p>Lucas Ciancaglini</p>
+			</div>
+		</div>
+	);
 }
 
 export default App;
